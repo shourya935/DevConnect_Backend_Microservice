290d81647036c127966c8ddb8209eaf1506b2f33
shourya93e <shouryajain333@gmail.com>
2025-10-07 20:26:00 +0530

    Setup new Modal (message.js) , Started building Real time Chat feature in DevConnect.

---
+++ src/models/message.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const  messageSchema = new mongoose.Schema({
+
+    senderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User"
+    },
+
+    receiverId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref:"User"
+    },
+
+    text: {
+        type: String
+    },
+
+    image: {
+        type: String
+    },
+},
+{timestamps: true}
+)
+
+const Message = mongoose.model("Message", messageSchema)
+
+module.exports = {Message}
